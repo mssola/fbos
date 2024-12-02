mssola/fbos

commit 32e880127366fabc3e8b4fecb642f1cc06b8fd71

Prepend the name of the task on debug
On 'printk' and 'sys_write' calls, prepend the name of the task that is
being executed for each message. This is an easy way to show off that we
are doing the right thing with the 'tp' register, even if we don't do
much with it.

Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

include/fbos/printk.h
@@ -11,7 +11,7 @@
 #ifdef __KERNEL__
 extern void die(const char *const message);
 extern void printk(const char *const message);
-extern void write(const char *const message, size_t n);
+extern void sys_write(const char *const message, size_t n);
 #else
 #include <stdio.h>
 #include <stdlib.h>

kernel/main.c
@@ -10,10 +10,10 @@ unsigned long stack[4][THREAD_SIZE / sizeof(unsigned long)];
 // Initialize the list of structs by providing a fixed stack address and empty
 // values everywhere else.
 struct task_struct tasks[4] = {
-	[TASK_INIT] = { .stack = stack[0], .entry_addr = nullptr, },
-	[TASK_FIZZ] = { .stack = stack[1], .entry_addr = nullptr, },
-	[TASK_BUZZ] = { .stack = stack[2], .entry_addr = nullptr, },
-	[TASK_FIZZBUZZ] = { .stack = stack[3], .entry_addr = nullptr, },
+	[TASK_INIT] = { .stack = stack[0], .name = "init", .entry_addr = nullptr, },
+	[TASK_FIZZ] = { .stack = stack[1], .name = "fizz", .entry_addr = nullptr, },
+	[TASK_BUZZ] = { .stack = stack[2], .name = "buzz", .entry_addr = nullptr, },
+	[TASK_FIZZBUZZ] = { .stack = stack[3], .name = "fizzbuzz", .entry_addr = nullptr, },
 };
 
 // Defined in fbos/init.h.

include/fbos/sched.h
@@ -12,6 +12,9 @@ enum task_id {
 	TASK_FIZZBUZZ = 3,
 };
 
+// Maximum length for the name of the process.
+#define TASK_NAME_LEN 16
+
 // All the information we need to grab for processes.
 struct task_struct {
 	// The stack allocated for the process. As you can see when initializing
@@ -20,6 +23,9 @@ struct task_struct {
 	// for simple `sp` values.
 	void *stack;
 
+	// Name of the task to be printed for debugging purposes.
+	const char name[TASK_NAME_LEN];
+
 	// The address for the binary entry.
 	const void *entry_addr;
 };
@@ -27,14 +33,26 @@ struct task_struct {
 // Tasks available on this kernel.
 extern struct task_struct tasks[4];
 
-// Set the return address to U-mode to the given task.
-__kernel __always_inline void set_return_address_to(int task_id)
+// Prepare for switching to the given task. Note that this will not actually
+// jump into the given task, but it will prepare the relevant registers before
+// performing the actual jump.
+__kernel __always_inline void prepare_switch_to(int task_id)
 {
+	register struct task_struct *current asm("tp");
+
 	asm volatile("csrc sstatus, %0\n\t"
 				 "mv t0, %1\n\t"
 				 "csrw sepc, t0" ::"r"(1 << 8),
 				 "r"(tasks[task_id].entry_addr)
 				 : "t0");
+
+	current = &tasks[task_id];
+
+	// Not really unused, but I was getting into lots of petty trouble with GCC
+	// depending if running with/without DEBUG on. Hence, let's set 'tp' in C
+	// instead of with inline assembly, even if the compiler thinks the variable
+	// is set but not used.
+	__unused(current);
 }
 
 #endif // __FBOS_SCHED_H_

kernel/head.S
@@ -94,9 +94,9 @@ _start_kernel:
 	li t6, 0
 	csrw sscratch, 0
 
-	// Point tp and sp to the init task. This is not crucial because in the end
-	// we are not doing anything with the 'tp' register, and our processes
-	// actually don't do anything on the stack.
+	// Point tp and sp to the init task. The 'tp' register will always point to
+	// the current process being executed, and it will be shown on debug when
+	// printing out messages.
 	la tp, tasks
 	la sp, tasks + THREAD_SIZE
 

kernel/printk.c
@@ -1,4 +1,5 @@
 #include <fbos/printk.h>
+#include <fbos/sched.h>
 #include <fbos/string.h>
 #include <fbos/sbi.h>
 
@@ -12,8 +13,40 @@ void __noreturn __kernel die(const char *const message)
 		;
 }
 
+__kernel void write(const char *const message, size_t n)
+{
+	struct sbi_ret ret = sbi_ecall2(DBCN_EXT, DBCN_WRITE, n, (unsigned long)message);
+	if (ret.error != SBI_SUCCESS) {
+		die(nullptr);
+	}
+}
+
+#ifdef __DEBUG__
+__kernel void print_task_prefix(void)
+{
+	register struct task_struct *current asm("tp");
+
+	if (!current) {
+		return;
+	}
+
+	size_t len = strlen(current->name);
+	if (!len) {
+		return;
+	}
+
+	write("[=> ", 4);
+	write(current->name, len);
+	write("] ", 2);
+}
+#endif
+
 __kernel void printk(const char *const message)
 {
+#ifdef __DEBUG__
+	print_task_prefix();
+#endif
+
 	size_t len = strlen(message);
 	if (!len) {
 		return;
@@ -22,10 +55,11 @@ __kernel void printk(const char *const message)
 	write(message, len);
 }
 
-__kernel void write(const char *const message, size_t n)
+__kernel void sys_write(const char *const message, size_t n)
 {
-	struct sbi_ret ret = sbi_ecall2(DBCN_EXT, DBCN_WRITE, n, (unsigned long)message);
-	if (ret.error != SBI_SUCCESS) {
-		die(nullptr);
-	}
+#ifdef __DEBUG__
+	print_task_prefix();
+#endif
+
+	write(message, n);
 }

kernel/trap.c
@@ -52,7 +52,7 @@ __kernel __always_inline void exception_handler(uint64_t cause)
 		die("Bad syscall\n");
 	}
 
-	write(message, n);
+	sys_write(message, n);
 }
 
 /*
@@ -75,7 +75,7 @@ __aligned(4) __s_interrupt __kernel void interrupt_handler(void)
 
 	if (IS_EXCEPTION(cause)) {
 		exception_handler(cause);
-		set_return_address_to(TASK_INIT);
+		prepare_switch_to(TASK_INIT);
 		goto end;
 	}
 
@@ -91,11 +91,11 @@ __aligned(4) __s_interrupt __kernel void interrupt_handler(void)
 		// BEHOLD! The fizz buzz logic! :D
 		seconds_elapsed += 1;
 		if ((seconds_elapsed % 15) == 0) {
-			set_return_address_to(TASK_FIZZBUZZ);
+			prepare_switch_to(TASK_FIZZBUZZ);
 		} else if ((seconds_elapsed % 5) == 0) {
-			set_return_address_to(TASK_BUZZ);
+			prepare_switch_to(TASK_BUZZ);
 		} else if ((seconds_elapsed % 3) == 0) {
-			set_return_address_to(TASK_FIZZ);
+			prepare_switch_to(TASK_FIZZ);
 		}
 
 		// Re-enable timer interrupts.

commit 7b1b8ebb48174445083bc7fa336dc00619cbf126

Don't repeat the task name when printing the model
When printing the initial model message on DEBUG it repeated the "init"
message twice. Avoid that by directly using 'write' on some of the
calls.

Fixes: 32e880127366 ("Prepend the name of the task on debug")

Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

include/fbos/printk.h
@@ -11,6 +11,7 @@
 #ifdef __KERNEL__
 extern void die(const char *const message);
 extern void printk(const char *const message);
+extern void write(const char *const message, size_t n);
 extern void sys_write(const char *const message, size_t n);
 #else
 #include <stdio.h>

kernel/main.c
@@ -2,6 +2,7 @@
 #include <fbos/printk.h>
 #include <fbos/mm.h>
 #include <fbos/sched.h>
+#include <fbos/string.h>
 #include <fbos/dt.h>
 
 // Stack to be used by our processes. "Blasphemy!" I hear you say. "How dare you
@@ -46,8 +47,8 @@ __noreturn __kernel void start_kernel(void *dtb)
 	// If we were able to fetch the model, print it now.
 	if (info.model[0] != '\0') {
 		printk("Running on: ");
-		printk(info.model);
-		printk("\n");
+		write(info.model, strlen(info.model));
+		write("\n", 1);
 	}
 
 	// At this point everything has already been handled: setup the interrupt