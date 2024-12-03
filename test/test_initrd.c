95c4b00e90e24d3bcb5d3b6e56f0e29b725ba9ce
Miquel Sabaté Solà <mikisabate@gmail.com>
2024-12-03 08:00:53 +0100

    test: Remove asserts on specific entry addrs
    These asserts where needed when I was iterating on the initrd handling
    code, but they are admittedly too specific and prone to errors. Hence,
    get rid off them.
    
    Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

--- test/test_initrd.c
+++ test/test_initrd.c
@@ -31,17 +31,10 @@ int main(void)
 	extract_initrd(contents, (uint64_t)fsize, tasks);
 	free(contents);
 
-#ifdef __DEBUG__
-	assert(((uintptr_t)tasks[0].entry_addr & 0xfff) == 0xbe8);
-	assert(((uintptr_t)tasks[1].entry_addr & 0xfff) == 0x5bc);
-	assert(((uintptr_t)tasks[2].entry_addr & 0xfff) == 0x7b4);
-	assert(((uintptr_t)tasks[3].entry_addr & 0xfff) == 0x180);
-#else
 	assert(tasks[0].entry_addr);
 	assert(tasks[1].entry_addr);
 	assert(tasks[2].entry_addr);
 	assert(tasks[3].entry_addr);
-#endif
 
 	exit(0);
 }
